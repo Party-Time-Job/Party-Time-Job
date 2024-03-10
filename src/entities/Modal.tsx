e45f0d8686de1f22a88a9599da13feaf96099f5c
clyde <clydehan0401@gmail.com>
2024-03-10 14:07:01 +0900

    feat: 모달 반응형 적용

--- src/entities/Modal.tsx
+++ src/entities/Modal.tsx
@@ -10,11 +10,11 @@ interface ModalComponentProps {
 const Modal = ({ modaltext, buttontext, onClick }: ModalComponentProps) => {
   return (
     <div className="fixed left-0 top-0 flex h-screen w-screen flex-col items-center justify-center bg-black opacity-75">
-      <div className="relative h-[250px] w-[540px] rounded-lg bg-white">
-        <Text className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-lg">
+      <div className="relative h-[220px] w-[370px] rounded-lg bg-white md:h-[250px] md:w-[540px]">
+        <Text className="absolute left-1/2 top-1/3 mt-5 -translate-x-1/2 -translate-y-1/2 transform text-lg md:top-1/2 md:mt-0">
           {modaltext}
         </Text>
-        <div className="absolute bottom-0 right-0 m-7">
+        <div className="absolute bottom-0 left-1/2 mb-7 -translate-x-1/2 transform md:absolute md:bottom-0 md:left-auto md:right-0 md:m-7 md:transform-none">
           <Button onClick={onClick} size="medium" type="active">
             {buttontext}
           </Button>
