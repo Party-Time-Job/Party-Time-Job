4c8697926073de3947f0a6cd6618dc93cf7556bf
yunsungLEE <chance07236@naver.com>
2024-03-11 20:01:20 +0900

    chore: 공통컴포넌트 Badge 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Badge from '@/shared/UI/Badge';
+
 export default function Home() {
   return (
     <>
       <h1>HomePage</h1>
+      <Badge type='approved'>승인완료</Badge>
+      <Badge type='pending'>대기중</Badge>
+      <Badge type='location'>서울시 성동구</Badge>
     </>
   );
 }
--- src/shared/ui/Badge.tsx
+++ src/shared/ui/Badge.tsx
@@ -1,15 +1,26 @@
+/**
+ * @param {string} children
+ * 버튼에 들어갈 내용을 입력시면 됩니다
+ * @param {string} type
+ * type은 총 3가지로 approved: 승인완료, pending: 대기중, location: 위치뱃지
+ * 기본값은 pendng으로 설정했습니다
+ */
+
 import Text from './Text';
 
 interface BadgeComponentProps {
   children: React.ReactNode;
-  width?: string;
-  color?: string;
+  type?: string;
 }
 
-const Badge = ({ children, width, color }: BadgeComponentProps) => {
+const Badge = ({ children, type = 'pending' }: BadgeComponentProps) => {
   return (
-    <div className={`${width} ${color}`}>
-      <Text>{children}</Text>
+    <div className='flex items-start gap-1'>
+      <div
+        className={`${type} flex items-center justify-center rounded-[20px] px-[10px] py-[6px]`}
+      >
+        <Text>{children}</Text>
+      </div>
     </div>
   );
 };
