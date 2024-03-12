75a63248d65cef8e111774f0cca377b76bd8f435
Crack_Egg <jaemyung123@naver.com>
2024-03-12 17:43:34 +0900

    Comment: 공통 버튼 컴포넌트에 대한 주석 추가

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,5 +1,17 @@
 import { ButtonInterface } from '@/shared/Button/type';
 
+/**
+ *
+ * @param {string} text 버튼에 들어갈 텍스트
+ * @param {'button' | 'submit'} type 버튼 타입(기본값은 button)
+ * @param {boolean} confirm 확인을 위한 버튼
+ * @param {'large' | 'medium' | 'mediumSmall' | 'small'} size 버튼 크기
+ * @param {'active' | 'inactive'} status 버튼 활성화 상태 표시
+ * @param {function} onClick 버튼 클릭 이벤트 핸들러
+ * @param {function} onSubmit 버튼 제출 이벤트 핸들러
+ * @returns 공통 버튼 컴포넌트
+ */
+
 const Button = ({
   text,
   type = 'button',
