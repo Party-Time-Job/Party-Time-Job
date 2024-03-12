8a161aa793ab3d7e42ea8c596f48e50c6d004686
yunsungLEE <chance07236@naver.com>
2024-03-12 19:29:42 +0900

    chore: 제작 컴포넌트 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import Filter from '@/features/Filter/Filter';
-import MyStore from '@/entities/Employer/MyStore';
-import Badge from '@/shared/UI/Badge';
-import RegisteredRecruitment from '@/entities/Employer/RegisteredRecruitment';
-
 export default function Home() {
   return (
     <>
       <h1>HomePage</h1>
-      <Badge>승인완료</Badge>
-      <Badge>대기중</Badge>
-      <Badge>서울시 성동구</Badge>
-      <Filter></Filter>
-      <MyStore></MyStore>
-      <RegisteredRecruitment></RegisteredRecruitment>
     </>
   );
 }
