5ed4a49ff163a3f624331a66452e553b886e9f22
Crack_Egg <jaemyung123@naver.com>
2024-03-12 18:02:40 +0900

    Style: 공통 타이틀 컴포넌트 스타일 추가

--- src/shared/Title/Title.tsx
+++ src/shared/Title/Title.tsx
@@ -10,9 +10,19 @@ const Title = ({
   children,
 }: TitleInterface) => {
   return (
-    <div>
-      {subtitle && <h2 style={{ fontSize: subSize }}>{subtitle}</h2>}
-      <h1 style={{ textAlign: align, fontSize: size, marginBottom: gap }}>
+    <div className='relative m-0 mx-auto h-full max-w-[964px] px-0 py-[60px] md:px-[12px] md:py-[40px] lg:w-full lg:max-w-none lg:px-[32px] lg:py-[60px]'>
+      {subtitle && (
+        <h2
+          style={{ fontSize: subSize }}
+          className='mb-2 text-base font-bold text-[#ea3c12]'
+        >
+          {subtitle}
+        </h2>
+      )}
+      <h1
+        style={{ textAlign: align, fontSize: size, marginBottom: gap }}
+        className='font-bold'
+      >
         {title}
       </h1>
       {children}
