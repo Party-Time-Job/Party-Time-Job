bff4e9954001f0295db47b11ba2510c84aca2392
Crack_Egg <jaemyung123@naver.com>
2024-03-13 15:31:31 +0900

    Style: SelectUi 스타일 추가

--- src/shared/ui/Select/ui/SelectUi.tsx
+++ src/shared/ui/Select/ui/SelectUi.tsx
@@ -11,20 +11,32 @@ const SelectUi = ({
   children,
 }: SelectUiInterface & Options) => {
   return (
-    <div>
-      <div ref={ref}>
-        {type === 'search' && title && <div>{title}</div>}
-        <button type='button' onClick={dropDown}>
+    <div className='z-10 w-full'>
+      <div
+        ref={ref}
+        className='flex w-full flex-col items-start justify-start gap-2'
+      >
+        {type === 'search' && title && (
+          <div className='text-base font-normal leading-[26px]'>{title}</div>
+        )}
+        <button type='button' onClick={dropDown} className='relative w-full'>
           {children}
         </button>
       </div>
-      <div>
-        <div>
+      <div className='relative w-full'>
+        <div className='relative'>
           {isOpen && (
-            <ul>
+            <ul className='absolute top-2 flex w-full max-w-[230px] flex-col items-start justify-center overflow-y-auto rounded-md border border-solid border-[#e5e4e7] bg-white shadow-md'>
               {options.map(option => (
-                <li key={option.value}>
-                  <button type='button' onClick={() => onClick(option.value)}>
+                <li
+                  key={option.value}
+                  className='h-fit w-full border-b border-b-black text-center'
+                >
+                  <button
+                    type='button'
+                    onClick={() => onClick(option.value)}
+                    className={`${type ? 'filter' : ''} h-full w-full px-0 py-3 text-sm font-normal leading-[22px]`}
+                  >
                     {option.value}
                   </button>
                 </li>
