ad7d68e9dbe64f01d9deab43574d296a9ab136a2
Crack_Egg <jaemyung123@naver.com>
2024-03-13 23:18:49 +0900

    Feat: 닫기 백그라운드 레이아웃 컴포넌트 구현

--- src/shared/BackgroundModal.tsx
+++ src/shared/BackgroundModal.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export interface BackgroundModalInterface {
@@ -5,3 +6,27 @@ export interface BackgroundModalInterface {
   onClick?: () => void;
   children: React.ReactNode;
 }
+
+const BackgroundModal = ({
+  size = 32,
+  onClick,
+  children,
+}: BackgroundModalInterface) => {
+  return (
+    <div>
+      <div>
+        <button type='button' onClick={onClick}>
+          <Image
+            src='/close.svg'
+            alt='닫기 아이콘'
+            width={size}
+            height={size}
+          />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundModal;
