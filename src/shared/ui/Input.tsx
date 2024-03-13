2c0ab7a4995295cc680e431b9282c5d91910f569
clyde <clydehan0401@gmail.com>
2024-03-13 14:59:13 +0900

    comment: JsDoc 설명 추가

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -4,6 +4,8 @@
  * @param {string} height
  * 테일윈드 규칙으로 작성해주시면 됩니다. 필수는 아닙니다.
  * @returns 사이즈에 맞는 인풋을 반환합니다.
+ *
+ * forwardRef를 사용하여 리액트 훅 폼의 register를 props로 받아 사용할 수 있도록 하였습니다.
  */
 
 import { forwardRef } from 'react';
