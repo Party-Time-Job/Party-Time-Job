a3d689623a8ba7b135be90c748568416140029b8
Crack_Egg <jaemyung123@naver.com>
2024-03-13 15:57:17 +0900

    Fix: dev 설정

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -4,11 +4,7 @@
  * @param {string} height
  * 테일윈드 규칙으로 작성해주시면 됩니다. 필수는 아닙니다.
  * @returns 사이즈에 맞는 인풋을 반환합니다.
- *
- * forwardRef를 사용하여 리액트 훅 폼의 register를 props로 받아 사용할 수 있도록 하였습니다.
  */
-
-import { forwardRef } from 'react';
 
 interface InputComponentProps {
   className?: string;
@@ -18,15 +14,25 @@ interface InputComponentProps {
   placeholder?: string;
   width?: string;
   height?: string;
-  id?: string;
 }
 
-const Input = forwardRef<HTMLInputElement, InputComponentProps>(
-  ({ className, width = 'w-80', height = '', ...rest }, ref) => {
-    const inputClassName = `${width} ${height} ${className || ''} flex items-start gap-2 self-stretch rounded-md border bg-white px-4 py-5`;
-
-    return <input {...rest} ref={ref} className={inputClassName} />;
-  },
-);
+const Input = ({
+  type,
+  value,
+  onChange,
+  placeholder,
+  width = 'w-80',
+  height,
+}: InputComponentProps) => {
+  return (
+    <input
+      className={`${width} ${height} flex items-start gap-2 self-stretch rounded-md border bg-white px-4 py-5`}
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+};
 
 export default Input;
