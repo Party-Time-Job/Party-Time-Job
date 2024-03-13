3b0bb76fc4a98493ca0b53109159a588399bf484
Crack_Egg <jaemyung123@naver.com>
2024-03-13 13:35:26 +0900

    Style: InputUi 스타일 추가

--- src/shared/ui/Input/ui/InputUi.tsx
+++ src/shared/ui/Input/ui/InputUi.tsx
@@ -19,19 +19,30 @@ const InputUi = forwardRef(
     ref: ForwardedRef<HTMLInputElement>,
   ) => {
     return (
-      <div>
-        {type === 'input' && <div>{title}</div>}
-        <div>
+      <div className='flex-start flex w-full flex-col justify-start gap-2'>
+        {type === 'input' && (
+          <div className='text-base font-normal leading-[26px]'>{title}</div>
+        )}
+        <div className='relative w-full'>
           <input
             type={isPassword ? 'password' : 'text'}
             placeholder={type === 'input' ? '입력' : title}
             defaultValue={defaultValue}
             onChange={onChange}
             ref={ref}
+            className='h-[58px] w-full rounded-[5px] border border-solid border-[#cbc9cf] px-5 py-4 text-base font-normal leading-[26px] text-black placeholder:text-[#a4a1aa]'
           />
-          {addition && <span>{addition}</span>}
+          {addition && (
+            <span className='translateY-50 absolute right-5 top-2/4 text-base font-normal leading-[26px]'>
+              {addition}
+            </span>
+          )}
         </div>
-        {isValid && <div>{valid}</div>}
+        {isValid && (
+          <div className='ml-2 text-xs font-normal leading-4 text-[#ff4040]'>
+            {valid}
+          </div>
+        )}
       </div>
     );
   },
