e99f8c26a9fe828a01fe36070c6ff9819146f5e7
Crack_Egg <jaemyung123@naver.com>
2024-03-13 23:19:59 +0900

    Comment: 닫기 백그라운드 레이아웃에 필요한 주석 추가

--- src/shared/BackgroundModal.tsx
+++ src/shared/BackgroundModal.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
 
+/**
+ * @param {number} size 버튼 크기 사이즈
+ * @param {function} onClick 닫기 클릭 이벤트 핸들러
+ * @param {ReactNode} children 안에 담겨질 내용
+ * @return 닫기 백그라운드 레이아웃
+ */
+
 export interface BackgroundModalInterface {
   size?: number;
   onClick?: () => void;
