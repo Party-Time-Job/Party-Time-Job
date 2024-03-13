0cf252e31adb66c0dc7ee2903d6d993aa22ab322
Crack_Egg <jaemyung123@naver.com>
2024-03-13 13:33:59 +0900

    Feat: Input 컴포넌트에 필요한 타입 지정
    - 한 곳에서 제어

--- src/shared/ui/Input/type.ts
+++ src/shared/ui/Input/type.ts
@@ -16,3 +16,21 @@ export interface ValidInterface {
   valid: string;
   isValid: boolean;
 }
+
+export type Type = {
+  type: 'input' | 'description';
+};
+
+export interface Option {
+  value: string;
+}
+
+export interface Options {
+  opitons: Option[];
+}
+
+// 한 곳에서 타입 제어
+export type InputProps = Type &
+  InputInterface &
+  Partial<Options> &
+  Partial<ValidInterface>;
