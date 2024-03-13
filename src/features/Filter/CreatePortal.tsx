41e3a8783dbc8adeb801781bc6621b840272a95c
Crack_Egg <jaemyung123@naver.com>
2024-03-13 23:29:09 +0900

    Comment: 포탈 생성에 필요한 타입 정의

--- src/features/Filter/CreatePortal.tsx
+++ src/features/Filter/CreatePortal.tsx
@@ -2,6 +2,12 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 
+/**
+ * @param {string} id 렌더링 대상 DOM 요소의 id
+ * @param {ReactNode} children 외부 DOM에 렌더링할 요소
+ * @return 외부 DOM에 렌더링
+ */
+
 export interface CreatePortalInterface {
   id: string;
   children: React.ReactNode;
