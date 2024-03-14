e52c10157fdbc94e378711488556c7e6c4c5ce97
Crack_Egg <jaemyung123@naver.com>
2024-03-14 19:32:03 +0900

    Feat: 메인 토스트 구현

--- src/shared/ui/Toast/Toast.tsx
+++ src/shared/ui/Toast/Toast.tsx
@@ -1,4 +1,17 @@
+import CreatePortal from '@/features/Filter/CreatePortal';
+import ShowToast from './ui/ShowToast';
+
 export interface ToastInterface {
   onShow: () => void;
   children: string;
 }
+
+const Toast = ({ onShow, children }: ToastInterface) => {
+  return (
+    <CreatePortal id='toast'>
+      <ShowToast onShow={onShow}>{children}</ShowToast>
+    </CreatePortal>
+  );
+};
+
+export default Toast;
