4bd4baa1181f767d32b593a14931944b8ad1e5bb
Crack_Egg <jaemyung123@naver.com>
2024-03-15 23:30:32 +0900

    Style: 테이블 body에 해당하는 스타일 추가

--- src/shared/ui/Table/ui/TableBodyUi.tsx
+++ src/shared/ui/Table/ui/TableBodyUi.tsx
@@ -6,13 +6,19 @@ export const TableBody = ({ children }: { children: React.ReactNode }) => {
 };
 
 export const TableBodyRow = ({ children }: { children: React.ReactNode }) => {
-  return <tr>{children}</tr>;
+  return (
+    <tr className='border-b border-b-[#cbc9cf] text-base font-normal md:text-sm md:font-normal'>
+      {children}
+    </tr>
+  );
 };
 
 export const TableBodyCell = ({ children }: { children: React.ReactNode }) => {
   return (
-    <td>
-      <div>{children}</div>
+    <td className='px-3 py-5 align-middle md:px-2 md:py-[9px]'>
+      <div className='hidden-webkit flex flex-row items-center justify-start gap-3 md:gap-2'>
+        {children}
+      </div>
     </td>
   );
 };
@@ -20,13 +26,29 @@ export const TableBodyCell = ({ children }: { children: React.ReactNode }) => {
 export const TableBodyStatus = ({ status }: { status: Status }) => {
   switch (status) {
     case 'pending':
-      return <div>대기중</div>;
+      return (
+        <div className='text-[#20a81e w-fit rounded-[20px] bg-[#d4f7d4] px-2.5 py-1.5 text-xs font-normal'>
+          대기중
+        </div>
+      );
     case 'accepted':
-      return <div>승인완료</div>;
+      return (
+        <div className='w-fit rounded-[20px] bg-[#cce6ff] px-2.5 py-1.5 text-xs font-normal text-[#0080ff]'>
+          승인완료
+        </div>
+      );
     case 'rejected':
-      return <div>거절</div>;
+      return (
+        <div className='w-fit rounded-[20px] bg-[#ffebe7;] px-2.5 py-1.5 text-xs font-normal text-[#ff4040]'>
+          거절
+        </div>
+      );
     case 'canceled':
-      return <div>취소</div>;
+      return (
+        <div className='w-fit rounded-[20px] bg-[#ffebe7;] px-2.5 py-1.5 text-xs font-normal text-[#ff4040]'>
+          취소
+        </div>
+      );
     default:
   }
 };
