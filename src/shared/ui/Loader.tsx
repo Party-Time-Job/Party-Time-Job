f94704e12185bb791310bbc80382a99781ee4d64
Crack_Egg <jaemyung123@naver.com>
2024-03-15 22:07:36 +0900

    Feat: 로딩 페이지에 해당하는 Ui 구현

---
+++ src/shared/ui/Loader.tsx
@@ -0,0 +1,26 @@
+import CreatePortal from '@/features/Filter/CreatePortal';
+
+const Loader = () => {
+  return (
+    <CreatePortal id='loader'>
+      <div>
+        <div>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    </CreatePortal>
+  );
+};
+
+export default Loader;
