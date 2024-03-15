11d183b02dca55ede3db973ee0cdd4fcbe600130
Crack_Egg <jaemyung123@naver.com>
2024-03-15 22:36:41 +0900

    Style: 스타일 추가

--- src/widgets/NoProfile/NoProfileRegister.tsx
+++ src/widgets/NoProfile/NoProfileRegister.tsx
@@ -7,9 +7,11 @@ export interface NoProfileRegisterInterface {
 
 const NoProfileRegister = ({ text, button }: NoProfileRegisterInterface) => {
   return (
-    <div>
-      <p>{text}</p>
-      <div>{button}</div>
+    <div className='rounded-12px] flex w-full flex-col items-center justify-center gap-[24px] border border-solid border-[#e5e4e7] pb-[60px] pt-[60px] md:gap-[16px]'>
+      <p className='text-base font-normal text-black md:text-sm md:font-normal'>
+        {text}
+      </p>
+      <div className='w-[346px] md:w-auto'>{button}</div>
     </div>
   );
 };
