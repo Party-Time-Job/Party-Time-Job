7955f1316db25fa8f5eb7ccf2cce0f8a601057ed
Crack_Egg <jaemyung123@naver.com>
2024-03-15 23:51:13 +0900

    Style: 테이블 컨테이너에 해당하는 스타일 추가

--- src/shared/ui/Table/ui/TableContainerUi.tsx
+++ src/shared/ui/Table/ui/TableContainerUi.tsx
@@ -8,9 +8,11 @@ const TableContainerUi = ({
   pagination: React.ReactNode;
 }) => {
   return (
-    <div>
-      <div>
-        <table>{children}</table>
+    <div className='relative w-full max-w-[964px] overflow-hidden rounded-[10px] border border-solid border-[#cbc9cf] bg-white'>
+      <div className='overflow-x-auto'>
+        <table className='odd:w-[200px] even:w-[280px] md:odd:w-[170px] md:even:w-[230px]'>
+          {children}
+        </table>
       </div>
       {pagination}
     </div>
