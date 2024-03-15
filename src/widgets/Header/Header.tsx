0197e3f719301d7c44f365b73010207b42b2ca62
clyde <clydehan0401@gmail.com>
2024-03-15 17:15:20 +0900

    feat: 반응형 구현

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,14 +1,27 @@
 const Header = () => {
   return (
-    <header className='fixed flex h-16 w-full items-center justify-center'>
-      <h1>로고입니다</h1>
-      <input
-        placeholder='가게 이름으로 찾아보세요'
-        className='h-10 w-[450px] rounded-md bg-gray-300 p-2'
-      />
-      <h1>로그인</h1>
-      <h1>회원가입</h1>
-      <div>알림아이콘</div>
+    <header className='fixed flex h-24 w-full items-center justify-center bg-white md:h-16'>
+      <div className='fixed flex justify-between md:w-[744px] md:px-9 lg:w-[1440px] lg:px-56'>
+        <div className='flex flex-col gap-3 md:flex-row md:items-center lg:gap-10'>
+          <div className='flex justify-between'>
+            <h5>로고입니다</h5>
+            <div className='flex items-center gap-3 md:hidden lg:gap-10'>
+              <h5>로그인</h5>
+              <h5>회원가입</h5>
+              <div>알림아이콘</div>
+            </div>
+          </div>
+          <input
+            placeholder='가게 이름으로 찾아보세요'
+            className='h-10 w-[344px] rounded-md bg-gray-300 p-2 lg:w-[450px]'
+          />
+        </div>
+        <div className='hidden items-center gap-3 md:flex lg:gap-10'>
+          <h5>로그인</h5>
+          <h5>회원가입</h5>
+          <div>알림아이콘</div>
+        </div>
+      </div>
     </header>
   );
 };
