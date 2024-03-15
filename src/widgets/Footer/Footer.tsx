8581883eb6f9cee59541994d1f0048f4fcc6f057
clyde <clydehan0401@gmail.com>
2024-03-16 00:52:34 +0900

    feat: 모바일 사이즈 구현

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -4,20 +4,22 @@ import CopyRight from './ui/CopyRight';
 
 const Footer = () => {
   return (
-    <footer className='flex h-[100px] items-center justify-between bg-[#F2F2F3] px-8 text-sm text-[#7D7986] md:px-8 lg:px-60'>
+    <footer className='flex h-[126px] items-center justify-between bg-[#F2F2F3] px-8 text-sm text-[#7D7986] md:h-[100px] md:px-8 lg:px-60'>
       <div className='hidden md:flex'>
         <CopyRight />
       </div>
       <div className='hidden gap-5 md:flex'>
         <PrivacyFAQ />
       </div>
-      <div className='w-full md:hidden'>
-        <div className='flex w-full items-center justify-between'>
-          <div className='flex gap-5'>
-            <PrivacyFAQ />
-          </div>
-          <div className='flex gap-2'>
-            <FooterIcon />
+      <div className='flex w-full flex-col gap-10 md:hidden'>
+        <div>
+          <div className='flex w-full items-center justify-between'>
+            <div className='flex gap-5'>
+              <PrivacyFAQ />
+            </div>
+            <div className='flex gap-2'>
+              <FooterIcon />
+            </div>
           </div>
         </div>
         <div className='md:hidden'>
