a49ca8c6ac1906357c1b8da042435cbfd7183593
Crack_Egg <jaemyung123@naver.com>
2024-03-15 22:39:51 +0900

    Feat: 프로필 없을 때 필요한 기능 구현

--- src/widgets/NoProfile/NoProfile.tsx
+++ src/widgets/NoProfile/NoProfile.tsx
@@ -1,3 +1,8 @@
+import Title from '@/shared/ui/Title';
+import NoProfileRegister from './NoProfileRegister';
+import RegisterButton from '../RegisterButton/RegisterButton';
+import ProfileContainer from '../ProfileContainer/ProfileContainer';
+
 export interface NoProfileInterface {
   isExist: boolean;
   profile?: {
@@ -7,3 +12,21 @@ export interface NoProfileInterface {
     bio: string;
   };
 }
+
+const NoProfile = ({ isExist, profile }: NoProfileInterface) => {
+  return (
+    <div>
+      <Title title='내 프로필' size={28} gap={32}>
+        {!isExist && (
+          <NoProfileRegister
+            text='내 프로필을 등록하고 원하는 가게에 지원해 보세요.'
+            button={<RegisterButton />}
+          />
+        )}
+        {isExist && profile && <ProfileContainer {...profile} />}
+      </Title>
+    </div>
+  );
+};
+
+export default NoProfile;
