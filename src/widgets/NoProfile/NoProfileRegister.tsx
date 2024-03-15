56e7c0d9654bffea3a85205ad069a3be2aedbe8a
Crack_Egg <jaemyung123@naver.com>
2024-03-15 22:36:16 +0900

    Feat: 프로필 없을 때 프로필 등록을 위한 창 구현

--- src/widgets/NoProfile/NoProfileRegister.tsx
+++ src/widgets/NoProfile/NoProfileRegister.tsx
@@ -4,3 +4,14 @@ export interface NoProfileRegisterInterface {
   text: string;
   button: ReactNode;
 }
+
+const NoProfileRegister = ({ text, button }: NoProfileRegisterInterface) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <div>{button}</div>
+    </div>
+  );
+};
+
+export default NoProfileRegister;
