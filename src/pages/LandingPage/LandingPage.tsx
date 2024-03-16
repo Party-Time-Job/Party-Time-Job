5e56438472ab481e54ede6d30f0b41be27f01ca1
clyde <clydehan0401@gmail.com>
2024-03-17 00:09:58 +0900

    feat: 랜딩페이지 헤더 반응형 구현

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -8,27 +8,12 @@ import EmployerCardList from './ui/EmployerCardList';
 import EmployeeTitle from './ui/EmployeeTitle';
 import EmployeeCardList from './ui/EmployeeCardList';
 import ServiceExplain from './ui/ServiceExplain';
+import LandingPageTopContents from './ui/LandingPageTopContents';
 
 const LandingPage = () => {
   return (
     <>
-      <section className='flex h-[800px] w-full items-center justify-center bg-black text-white'>
-        <div className='flex w-[900px] flex-col px-12'>
-          <div>
-            <Text as='h2' className='text-xl font-light'>
-              당신의 알바 구인구직
-            </Text>
-            <Text as='h2' className='text-xl font-light'>
-              이제는 파티처럼 신나게!
-            </Text>
-          </div>
-          <div className='flex items-end gap-8'>
-            <TextLogo />
-            <LandingLogo />
-          </div>
-        </div>
-        <img src='test-main.png' className='w-[300px]' />
-      </section>
+      <LandingPageTopContents />
 
       <section className='w-full bg-black px-12'>
         <div className='flex justify-center'>
