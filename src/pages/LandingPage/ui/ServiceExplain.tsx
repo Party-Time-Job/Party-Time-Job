59ef7ed3f37397f43539f32531560c899e806684
clyde <clydehan0401@gmail.com>
2024-03-17 00:02:33 +0900

    feat: 서비스 설명 섹션 반응형 구현

--- src/pages/LandingPage/ui/ServiceExplain.tsx
+++ src/pages/LandingPage/ui/ServiceExplain.tsx
@@ -3,12 +3,25 @@ import Text from '@/shared/ui/Text';
 const ServiceExplain = () => {
   return (
     <>
-      <div className='flex flex-col items-center'>
+      <div className='hidden flex-col items-center lg:flex'>
         <Text as='p'>
           구인구직 과정을 파티처럼 즐겁고, 더욱 쉽게 만들고자 합니다.
         </Text>
         <Text as='p'>
           당신의 다음 파티는 어디에서 시작될까요? 지금 바로 시작해 보세요!
+        </Text>
+      </div>
+
+      <div className='flex flex-col items-center gap-2 text-center lg:hidden'>
+        <Text as='p'>
+          구인구직 과정을 파티처럼 즐겁고
+          <br />
+          더욱 쉽게 만들고자 합니다.
+        </Text>
+        <Text as='p'>
+          당신의 다음 파티는 어디에서 시작될까요?
+          <br />
+          지금 바로 시작해 보세요!
         </Text>
       </div>
     </>
