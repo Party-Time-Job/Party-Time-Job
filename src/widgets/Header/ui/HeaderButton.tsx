f26f741a01f7cc75bddbd9f83f3475a6216715ac
clyde <clydehan0401@gmail.com>
2024-03-17 01:46:52 +0900

    feat: 헤더 디자인 변경

--- src/widgets/Header/ui/HeaderButton.tsx
+++ src/widgets/Header/ui/HeaderButton.tsx
@@ -7,7 +7,7 @@ const HeaderButton = ({ text, onClick }: ButtonInterface) => {
   return (
     <button
       onClick={onClick}
-      className='border-test-green hover:bg-test-green rounded-xl border-2 px-3 py-1 text-sm font-bold text-white transition-all duration-300 hover:font-bold hover:text-black'
+      className='border-test-green hover:bg-test-green rounded-xl border px-3 py-1 text-sm font-light text-white transition-all duration-300 hover:font-bold hover:text-black'
     >
       {text}
     </button>
