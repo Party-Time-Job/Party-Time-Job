63aa228f6a966b90c645c403b1950761cdbe09ac
Crack_Egg <jaemyung123@naver.com>
2024-03-17 17:14:57 +0900

    Feat: 신청 내역 없을 때 필요한 박스 구현
    - 스타일 추가

---
+++ src/widgets/Application/ui/NoRegisterUi.tsx
@@ -0,0 +1,7 @@
+const NoRegisterUi = () => {
+  return (
+    <div className='relative mx-auto my-0 h-[460px] max-w-[964px] bg-white px-0 py-[60px] md:h-[330px] md:w-full md:px-[12px] md:py-[40px] lg:w-full lg:max-w-none lg:px-[32px] lg:py-[60px]' />
+  );
+};
+
+export default NoRegisterUi;
