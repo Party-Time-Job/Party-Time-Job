cc85aa8eb23c74cfe80f7ec4440a55794c3bf5e8
Crack_Egg <jaemyung123@naver.com>
2024-03-17 16:49:57 +0900

    Style: 페이지네이션 UI에 필요한 스타일 추가

--- src/shared/ui/Pagination/ui/PaginationUi.tsx
+++ src/shared/ui/Pagination/ui/PaginationUi.tsx
@@ -23,19 +23,33 @@ const PaginationUi = ({
     : `${pathName}?page=`;
 
   return (
-    <div>
-      <div>
-        <Link href={`${url}${page - 1}`}>
+    <div className='flex flex-col items-center justify-center'>
+      <div className='mb-3 mt-3 flex flex-col items-center justify-center gap-5'>
+        <Link
+          href={`${url}${page - 1}`}
+          className={`${isPrevious ? '' : 'pointer-events-none cursor-default'}`}
+        >
           <ArrowUi isActive={isPrevious} direction='previous' />
         </Link>
-        <div>
+        <div className='flex flex-col items-center justify-center gap-0.5 text-sm font-normal  md:gap-1 md:text-xs md:font-normal'>
           {showed.map(num => (
-            <Link key={num} href={`${url}${num}`}>
-              <span>{num}</span>
+            <Link
+              key={num}
+              href={`${url}${num}`}
+              className={`${page === num ? '' : 'pointer-events-none cursor-default'}`}
+            >
+              <span
+                className={`${page === num ? '' : 'bg-[#6b8a8c] text-white'} flex h-[40px] w-[40px] flex-col items-center justify-center rounded text-black md:h-[32px] md:w-[32px]`}
+              >
+                {num}
+              </span>
             </Link>
           ))}
         </div>
-        <Link href={`${url}${page + 1}`}>
+        <Link
+          href={`${url}${page + 1}`}
+          className={`${isNext ? '' : 'pointer-events-none cursor-default'}`}
+        >
           <ArrowUi isActive={isNext} direction='next' />
         </Link>
       </div>
