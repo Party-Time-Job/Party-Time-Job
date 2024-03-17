d0aebafa519f5a9ce053e45f0886a4173ae88b2b
siyeol97 <leesi2830@naver.com>
2024-03-17 14:27:04 +0900

    comment: JsDoc 주석 추가

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -2,7 +2,12 @@ import { getMethod } from '@/shared/api/RequestMethod';
 import { AllNotice } from '@/entities/Post/types';
 import NoticeList from '@/entities/Notice/NoticeList';
 import getSearchList from './getSearchList.ts';
-// result.items.item.shop.item.name
+
+/**
+ *
+ * @param {string} keyword 검색어
+ * @returns 검색어에 해당하는 공고 리스트
+ */
 const SearchPage = async ({ keyword }: { keyword: string }) => {
   const noticeItemList = await getMethod<AllNotice>(
     'https://bootcamp-api.codeit.kr/api/3-2/the-julge/notices',
--- src/pages/searchPage/getSearchList.ts
+++ src/pages/searchPage/getSearchList.ts
@@ -1,5 +1,11 @@
 import { Notice } from '@/entities/Post/types';
 
+/**
+ *
+ * @param {Notice[]} noticeItemList 전체 공고 리스트
+ * @param {string} keyword 검색어
+ * @returns 검색어에 해당하는 공고 리스트
+ */
 const getSearchList = (noticeItemList: Notice[], keyword: string) => {
   const searchResult = noticeItemList.filter(notice => {
     return notice.item.shop.item.name.includes(keyword);
