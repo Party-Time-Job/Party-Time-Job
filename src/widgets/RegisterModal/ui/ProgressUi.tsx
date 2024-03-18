d8813ec9c66e3169b08299ceefeb4e0fbb26846c
Crack_Egg <jaemyung123@naver.com>
2024-03-18 16:48:51 +0900

    Style: 프로그레스 Ui에 필요한 스타일 추가

--- src/widgets/RegisterModal/ui/ProgressUi.tsx
+++ src/widgets/RegisterModal/ui/ProgressUi.tsx
@@ -9,8 +9,11 @@ const ProgressUi = ({
   const progress = (steps / (entire.length - 1)) * 100;
 
   return (
-    <div>
-      <div style={{ width: `${progress}%` }} />
+    <div className='relative mx-auto mb-[5px] mt-0 h-[8px] w-[98%] overflow-hidden rounded bg-[#cbc9cf]'>
+      <div
+        style={{ width: `${progress}%` }}
+        className='h-full rounded bg-[#6b8a8c] duration-300 ease-in-out'
+      />
     </div>
   );
 };
