0be6c8aa413f86913c00e0f289bc2aa430985f06
Crack_Egg <jaemyung123@naver.com>
2024-03-19 17:15:25 +0900

    Style: 스타일 수정

--- src/shared/NotificationModal/NotificationModal.tsx
+++ src/shared/NotificationModal/NotificationModal.tsx
@@ -23,14 +23,10 @@ const NotifiactionModal = ({
   };
 
   return (
-    <div className='z-1000 right-350 rounded-10 relative top-12 w-[368px] overflow-y-auto border border-solid border-[#cbc9cf] bg-[#ffebe7] px-20 py-0 pb-24 shadow-md md:fixed md:inset-0 md:w-full md:rounded-none md:border-0 md:shadow-none'>
+    <div className='z-1000 right-350 rounded-10 relative top-12 w-[368px] overflow-y-auto border border-solid border-[#cbc9cf] bg-[#ffebe7] px-20 py-0 pb-24 shadow-md'>
       <div className='bg-#ffebe7] sticky top-0 flex justify-between pb-16 pt-24'>
         <h1 className='text-lg font-bold'>{`알림 ${items.length}개`}</h1>
-        <button
-          type='button'
-          onClick={handleClose}
-          className='md:top-26 hidden md:right-20 md:block'
-        >
+        <button type='button' onClick={handleClose}>
           <Image src={'/close.svg'} alt='닫기 아이콘' width={24} height={24} />
         </button>
       </div>
