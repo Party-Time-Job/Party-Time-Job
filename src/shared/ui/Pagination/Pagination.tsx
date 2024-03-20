56d93831827f733b240d3482469a6f865e839f23
Crack_Egg <jaemyung123@naver.com>
2024-03-20 20:23:55 +0900

    Comment: 주석 제거

--- src/shared/ui/Pagination/Pagination.tsx
+++ src/shared/ui/Pagination/Pagination.tsx
@@ -19,15 +19,10 @@ const Pagination = ({
   showedItems: number;
   total: number;
 }) => {
-  // 현재 페이지가 속한 그룹 변수
   const currentNumber = Math.floor((page - 1) / MAX_PAGE) + 1;
-  // 마지막 페이지 번호
   const finalNumber = Math.floor((total - 1) / showedItems) + 1;
-  // 현재 그룹에서 보여지는 시작과 끝
   const currentStartNumber = Math.floor((page - 1) / MAX_PAGE) * MAX_PAGE + 1;
   const currentFinalNumber = Math.min(finalNumber, currentNumber * MAX_PAGE);
-  // 현재 보여지는 그룹 -> 배열 형태로 저장
-  // 값 필요 X
   const pageGroup = Array.from(
     { length: currentFinalNumber - currentStartNumber + 1 },
     (_, i) => i + currentStartNumber,
