bf8ce4e543d7caf244ca911d1ec435b0409d38df
Crack_Egg <jaemyung123@naver.com>
2024-03-21 15:51:20 +0900

    Refactor: 프로필 페이지 일자 수정

--- src/shared/utils/formatTime.ts
+++ src/shared/utils/formatTime.ts
@@ -1,12 +1,17 @@
 const formatTime = (startsAt: string, workhour: number) => {
-  const startDate = startsAt.slice(0, 10);
-  const startHour = startsAt.slice(11, 13);
-  const startMinute = startsAt.slice(14, 16);
-  const finishedHour = (Number(startHour) + workhour) % 24;
-  const formatFinishedHour =
-    finishedHour < 10 ? `0${finishedHour}` : finishedHour;
-  const now = `${formatFinishedHour}:${startMinute}`;
-  return `${startDate} ${startHour}:${startMinute}~${now}`;
+  const startDate = new Date(startsAt);
+  const endDate = new Date(startDate.getTime() + workhour * 60 * 60 * 1000);
+  const startYear = startDate.getFullYear();
+  const getMonth = startDate.getMonth() + 1;
+  const startMonth = `${getMonth < 10 ? '0' : ''}${getMonth}`;
+  const startDay = startDate.getDate();
+  const startHours = startDate.getHours();
+  const startMinutes = startDate.getMinutes();
+  const newStartDate = `${startHours < 10 ? '0' : ''}${startHours}:${startMinutes < 10 ? '0' : ''}${startMinutes}`;
+  const endHours = endDate.getHours();
+  const endMinutes = endDate.getMinutes();
+  const newEndDate = `${endHours < 10 ? '0' : ''}${endHours}:${endMinutes < 10 ? '0' : ''}${endMinutes}`;
+  return `${startYear}-${startMonth}-${startDay} ${newStartDate} ~ ${newEndDate} (${workhour}시간)`;
 };
 
 export default formatTime;
