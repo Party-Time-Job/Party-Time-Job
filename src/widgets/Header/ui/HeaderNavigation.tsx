530847a620a3accfe5b4ec3053c0114e9ac2e628
clyde <clydehan0401@gmail.com>
2024-03-21 17:30:27 +0900

    feat: 사장 유저 알림 아이콘 삭제

--- src/widgets/Header/ui/HeaderNavigation.tsx
+++ src/widgets/Header/ui/HeaderNavigation.tsx
@@ -69,7 +69,6 @@ const HeaderNavigation = () => {
         <>
           <MyStoreLink />
           <LogoutButton />
-          <NotificationIcon />
         </>
       )}
     </>
--- src/widgets/Header/ui/NotificationIcon.tsx
+++ src/widgets/Header/ui/NotificationIcon.tsx
@@ -1,6 +1,6 @@
 // 알림 관련 기능을 구현하시면 active-notification.svg를 사용하여 알림 로직을 구현해주세요.
 const NotificationIcon = () => {
-  return <img src='/notification.svg' alt='알림' />;
+  return <img src='/notification.svg' className='bg-white' alt='알림' />;
 };
 
 export default NotificationIcon;
