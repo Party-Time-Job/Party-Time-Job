301ea856d0724469e5e65f818cc51c07e6ffc6d0
clyde <clydehan0401@gmail.com>
2024-03-24 05:27:04 +0900

    feat: 랜딩페이지 메인이미지 추가

--- src/page/LandingPage/ui/LandingPageTopContents.tsx
+++ src/page/LandingPage/ui/LandingPageTopContents.tsx
@@ -5,8 +5,8 @@ import TextLogo from '@/shared/ui/TextLogo';
 const LandingPageTopContents = () => {
   return (
     <>
-      <section className='hidden h-[800px] w-full items-center justify-center bg-black text-white lg:flex'>
-        <div className='flex w-[900px] flex-col px-12'>
+      <section className='relative hidden h-[800px] w-full items-center justify-center bg-black text-white lg:flex'>
+        <div className='z-10 flex w-[900px] flex-col px-12'>
           <div>
             <Text as='h2' className='text-xl font-light'>
               당신의 알바 구인구직
@@ -20,7 +20,13 @@ const LandingPageTopContents = () => {
             <LandingLogo />
           </div>
         </div>
-        <img src='test-main.png' className='w-[300px]' />
+        <section className='flex'>
+          <img
+            src='main-image.png'
+            alt='main-image'
+            className='absolute left-64 top-0 z-0 h-[900px] w-full object-contain opacity-55'
+          />
+        </section>
       </section>
 
       <section className='flex h-[800px] w-full items-center justify-center bg-black text-white lg:hidden'>
