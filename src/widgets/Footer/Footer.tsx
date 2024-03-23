bdefd923bc8b60a1ddfb4ddf65fbd7511d3e8643
clyde <clydehan0401@gmail.com>
2024-03-24 03:01:32 +0900

    feat: 로그인, 회원가입 페이지 헤더,푸터 제외

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import FooterIcon from './ui/FooterIcon';
 import PrivacyFAQ from './ui/PrivacyFAQ';
 import CopyRight from './ui/CopyRight';
@@ -6,6 +9,16 @@ import CopyRight from './ui/CopyRight';
  * 푸터에는 아무 기능도 넣지 않았고, ui만 구현했습니다.
  */
 const Footer = () => {
+  const pathname = usePathname();
+
+  if (pathname === '/login') {
+    return null;
+  }
+
+  if (pathname === '/signup') {
+    return null;
+  }
+
   return (
     <footer className='flex h-[126px] items-center justify-between bg-black px-8 text-sm text-[#7D7986] md:h-[100px] md:px-8 lg:px-60'>
       <div className='hidden md:flex'>
