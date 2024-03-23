53aa357d9b33196c864515a13f2ba304670f9ac7
yunsungLEE <chance07236@naver.com>
2024-03-24 07:24:01 +0900

    chore: 임시 커밋

--- src/features/Create-Shop/CreateShop.tsx
+++ src/features/Create-Shop/CreateShop.tsx
@@ -33,11 +33,26 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
     register,
     handleSubmit,
     reset,
+    // getValues,
     formState: { isSubmitting, errors },
   } = useForm<FieldValues>({
     defaultValues: initialValues,
   });
+  let uploadImage;
   const router = useRouter();
+  // const reader = new FileReader();
+  // imageRef.current = file;
+  // reader.onload = (e: ProgressEvent<FileReader>) => {
+  //   if (e.target?.result) {
+  //     setImageUrl(e.target.result.toString());
+  //   }
+  // };
+  // reader.readAsDataURL(file);
+  // const handleChange = () => {
+  //   const { imageUrl } = getValues();
+  //   console.log(imageUrl[0].name);
+  //   uploadImage = imageUrl[0].name;
+  // };
   const url = shopId ? `${baseUrl}/shops/${shopId}` : `${baseUrl}/shops`;
   const method = shopId ? 'PUT' : 'POST';
   const requestInfo = async (data: FieldValues): Promise<void> => {
@@ -51,7 +66,10 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({
+          ...data,
+          imageUrl: data.imageUrl[0].name,
+        }),
       });
       if (response.status === 200) {
         router.push('/shop/details');
@@ -64,22 +82,24 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
     reset(initialValues);
   }, []);
   return (
-    <div className='flex flex-col items-start gap-2 px-[238px] py-[60px]'>
+    <div className='flex flex-col items-center gap-2 px-[238px] py-[60px]'>
       <div className='flex flex-col items-center gap-8'>
         <div className='flex w-[964px] items-center justify-between'>
           <Title title='가게정보'>
             <div></div>
           </Title>
-          <Image src={'/close.svg'} alt='close' width={32} height={32} />
+          <Image src={'/close-white.svg'} alt='close' width={32} height={32} />
         </div>
         {/* 여기서부터 폼 태그 */}
         <form
           onSubmit={handleSubmit(data => requestInfo(data))}
-          // onSubmit={(e) => {
-          //   e.preventDefault();
-          //   console.log(URL);
-          // }}
-          className='flex h-[869px] w-[964px] flex-col gap-6'
+          // onSubmit={handleSubmit(data =>
+          //   console.log({
+          //     ...data,
+          //     imageUrl: data.imageUrl[0].name,
+          //   }),
+          // )}
+          className='flex w-[964px] flex-col gap-6'
         >
           <div className='flex w-[964px] items-start gap-5'>
             {/* 가게 이름 */}
@@ -100,14 +120,28 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
             {/* 분류 */}
             <div className='flex w-[472px] flex-shrink-0 flex-col items-start gap-2'>
               <label htmlFor='category'>분류</label>
-              <select
-                className='flex w-[100%] items-start gap-2 self-stretch rounded-md bg-test-black px-4 py-5 text-white'
-                {...register('category')}
-              >
-                {CLASSIFICATION.map(item => (
-                  <option key={item.key}>{item.value}</option>
-                ))}
-              </select>
+              <div className='relative w-[100%]'>
+                <select
+                  className='flex w-[100%] appearance-none items-start gap-2 self-stretch rounded-md bg-test-black bg-right bg-no-repeat px-4 py-5 pl-3 pr-10 text-white'
+                  {...register('category')}
+                >
+                  {CLASSIFICATION.map(item => (
+                    <option key={item.key}>{item.value}</option>
+                  ))}
+                </select>
+                <svg
+                  className='pointer-events-none absolute right-2 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </div>
               {errors.category && (
                 <span>{errors.category.message?.toString()}</span>
               )}
@@ -117,14 +151,28 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
             {/* 주소 */}
             <div className='flex w-[472px] flex-shrink-0 flex-col items-start gap-2'>
               <label htmlFor='address1'>주소</label>
-              <select
-                className='flex w-[100%] items-start gap-2 self-stretch rounded-md bg-test-black px-4 py-5 text-white'
-                {...register('address1')}
-              >
-                {ADDRESS.map(item => (
-                  <option key={item.key}>{item.value}</option>
-                ))}
-              </select>
+              <div className='relative w-[100%]'>
+                <select
+                  className='flex w-[100%] appearance-none items-start gap-2 self-stretch rounded-md bg-test-black bg-right bg-no-repeat px-4 py-5 pl-3 pr-10 text-white'
+                  {...register('address1')}
+                >
+                  {ADDRESS.map(item => (
+                    <option key={item.key}>{item.value}</option>
+                  ))}
+                </select>
+                <svg
+                  className='pointer-events-none absolute right-2 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </div>
               {errors.address1 && (
                 <span>{errors.address1.message?.toString()}</span>
               )}
@@ -162,32 +210,38 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
             )}
           </div>
           {/* 가게 이미지 추가하기  */}
-          <div className='flex h-[500px] w-[472px] flex-col items-start gap-2'>
+          <div className='flex h-[400px] w-[472px] flex-col items-start gap-2'>
             <div>가게 이미지</div>
             <div className='h-full w-full'>
               <label htmlFor='imageUrl'>
-                <div className='flex h-full flex-col items-center justify-center bg-pt-gray20'>
-                  <Image
-                    width={32}
-                    height={32}
-                    src={'/camera.svg'}
-                    alt='camera'
-                  />
-                  <Text
-                    as='p'
-                    className='text-center font-bold leading-5 text-[#A4A1AA]'
-                  >
-                    이미지 추가하기
-                  </Text>
+                <div className='flex h-full flex-col items-center justify-center bg-test-black'>
+                  <div className='flex w-full flex-col justify-center'>
+                    <div className='mx-auto'>
+                      <Input
+                        type='image'
+                        src={uploadImage || '/camera.svg'}
+                        alt='camera'
+                        className='fill'
+                      />
+                    </div>
+                    <Text
+                      as='p'
+                      className='text-center font-bold leading-5 text-[#A4A1AA]'
+                    >
+                      이미지 추가하기
+                    </Text>
+                  </div>
                 </div>
               </label>
             </div>
             <Input
+              className='w-[100%]'
               id='imageUrl'
-              type='text'
+              type='file'
               {...register('imageUrl', {
                 required: '이미지를 추가해주세요.',
               })}
+              // onChange={handleChange}
             />
             {errors.imageUrl && (
               <span>{errors.imageUrl.message?.toString()}</span>
@@ -198,7 +252,7 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
             <label htmlFor='description'>가게 설명</label>
             <textarea
               id='description'
-              className='w-full place-content-center p-2.5'
+              className='w-full place-content-center bg-test-black p-2.5'
               placeholder='입력'
               rows={8}
               cols={30}
@@ -211,7 +265,7 @@ const CreateShop = ({ initialValues, shopId }: CreateShopProps) => {
             <Button
               size='medium'
               status={isSubmitting ? 'inactive' : 'active'}
-              text={shopId === null ? '정보 수정하기' : '가게 등록하기'}
+              text={shopId ? '정보 수정하기' : '가게 등록하기'}
               disabled={isSubmitting}
               type='submit'
             />
