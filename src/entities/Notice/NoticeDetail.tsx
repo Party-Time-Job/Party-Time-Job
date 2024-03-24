2c22e00438e8c2afb3d6510bdb0d082f86be8068
Crack_Egg <jaemyung123@naver.com>
2024-03-24 23:58:20 +0900

    Refactor: 불필요한 로더 수정

--- src/entities/Notice/NoticeDetail.tsx
+++ src/entities/Notice/NoticeDetail.tsx
@@ -6,7 +6,6 @@ import useDetailNotice from './hooks/useDetailNotice.ts';
 import useApplication from './hooks/useApplication.ts';
 import useNoticeStatus from './hooks/useNoticeStatus.ts';
 import NoticeDescription from './NoticeDescription.tsx';
-import Loader from '@/shared/ui/Loader.tsx';
 
 /**
  * @param {Object} props - NoticeDetail 컴포넌트의 props
@@ -30,7 +29,7 @@ const NoticeDetail = ({
 
   return (
     <section className='flex w-full items-center justify-center px-[12px] py-[40px] md:px-[32px] md:py-[60px]'>
-      {detail ? (
+      {detail && (
         <div className='flex w-full flex-col gap-4 lg:w-[964px]'>
           <div className='flex flex-col gap-2'>
             <span className='flex h-8 w-14 items-center justify-center rounded-lg bg-test-green text-sm font-bold text-black'>
@@ -55,8 +54,6 @@ const NoticeDetail = ({
             <NoticeDescription detail={detail} />
           </div>
         </div>
-      ) : (
-        <Loader />
       )}
     </section>
   );
