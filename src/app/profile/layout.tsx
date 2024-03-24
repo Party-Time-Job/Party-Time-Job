593b9257dca0646e689b71245cc969fdfbd5bfae
Crack_Egg <jaemyung123@naver.com>
2024-03-25 00:04:32 +0900

    Fix: 컴포넌트로 수정

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,28 +1,12 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Loader from '@/shared/ui/Loader';
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   const [showSpinner, setShowSpinner] = useState(true);
 
-  const LoadingSpinner = () => (
-    <div className='fixed top-0 flex h-full w-full items-center justify-center bg-black'>
-      <div className='relative inline-block h-[80px] w-[80px]'>
-        <div className='absolute left-[66px] top-[37px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[62px] top-[22px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[52px] top-[11px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[37px] top-[7px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[22px] top-[11px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[11px] top-[22px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[7px] top-[37px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[11px] top-[52px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[22px] top-[62px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[37px] top-[66px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[52px] top-[62px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-        <div className='absolute left-[62px] top-[52px] h-1.5 w-1.5 animate-[scale_1.2s_linear_infinite] rounded-[50%] bg-green-400 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]' />
-      </div>
-    </div>
-  );
+  const LoadingSpinner = () => <Loader />;
 
   useEffect(() => {
     const timer = setTimeout(() => setShowSpinner(false), 2000);
